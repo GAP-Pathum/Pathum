4bf7ef4198b2095dafd485fb828c6757f7f99547
GAP-Pathum <pathumpasindu41@gmail.com>
2024-11-22 08:32:04 +0530

    base created

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',  // Adjust if needed for sub-paths
+  base: '/Pathum/',  // Adjust if needed for sub-paths
   server: {
     fs: {
       allow: ['.'], // Allows all files in the root project to be served
