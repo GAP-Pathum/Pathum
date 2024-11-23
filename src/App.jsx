ab4f94f04a59a4b5ea746f89e6eef37ca0d342c3
GAP-Pathum <pathumpasindu41@gmail.com>
2024-11-23 08:52:43 +0530

    Refactor ScrollToTop component and update routing to use React Router v6

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,22 @@
-// src/App.js
-import React, { useContext } from 'react';
-import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { Main, BlogPage, ProjectPage } from './pages';
 import { BackToTop } from './components'; 
 import ScrollToTop from './utils/ScrollToTop';
 import './App.css';
 
 function App() {
-
   return (
     <div className="app">
       <Router>
         <ScrollToTop />
-        <Switch>
-          <Route path="/" exact component={Main} />
-          <Route path="/blog" exact component={BlogPage} />
-          <Route path="/projects" exact component={ProjectPage} />
-          <Redirect to="/" />
-        </Switch>
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/blog" element={<BlogPage />} />
+          <Route path="/projects" element={<ProjectPage />} />
+          {/* Redirect to homepage */}
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
       </Router>
       <BackToTop />
     </div>
